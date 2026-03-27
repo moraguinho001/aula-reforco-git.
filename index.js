ba521b6fab1ba8f1c7ec988e3d4c82aca96fa0db
mariane <motta.mariane@escola.pr.gov.br>
2026-03-27 11:54:14 -0300

    Meu primeiro Hello World no Git

---
+++ index.js
@@ -0,0 +1,7 @@
+console . log (" Fluxo Git c o n c l u d o com sucesso !") ;
+# 1. Avisa o Git para observar as mudancas
+git add .
+# 2. Cria uma " foto " salva do seu progresso
+git commit -m "Meu primeiro Hello World no Git "
+# 3. Envia para o site do GitHub
+git push
